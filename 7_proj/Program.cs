81f63e9a698169953b26506435c6c55b0b08dd55
Ivan <vanya1254@yandex.ru>
2022-10-13 15:52:11 +0300

    add 7_proj

---
+++ 7_proj/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (max < num2)
+{
+    max = num2;
+}
+if (max < num3)
+{
+    max = num3;
+}
+Console.Write($"Большее число {max}");
