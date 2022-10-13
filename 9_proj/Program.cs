baab4407a45cd9eedb9444cf594bd36b881c1a7a
Ivan <vanya1254@yandex.ru>
2022-10-13 16:22:30 +0300

    add 9_proj

---
+++ 9_proj/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if (num >= count)
+{
+    while (num >= count)
+    {
+        Console.Write($"{count}; ");
+        count = count + 2;
+    }
+}
+else
+{
+    Console.Write($"Нет четных чисел в диапазоне от 1 до {num}");
+}
