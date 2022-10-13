f32eb7f3ba61e0134fcfef3550a6fbeb7372c4c6
Ivan <vanya1254@yandex.ru>
2022-10-13 12:41:02 +0300

    add 5_proj

---
+++ 5_proj/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int lastDigit = num % 10;
+Console.WriteLine($"Последняя цифра числа равна {lastDigit}");
