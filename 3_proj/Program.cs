b08c1cbcaa14888eda36a8deb6364f2a3874a98d
Ivan <vanya1254@yandex.ru>
2022-10-13 12:05:23 +0300

    add 3_proj

---
+++ 3_proj/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Введите число : ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day < 8 )
+{
+    if (day == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+     if (day == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+     if (day == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+     if (day == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+     if (day == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+     if (day == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+     if (day == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+}
+else
+{  
+    Console.WriteLine("Заданное число не соответствует дню недели");
+}
