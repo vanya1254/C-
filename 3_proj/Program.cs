dc1e546c7633765d2f3814c85be3bf0fefb365e3
Ivan <vanya1254@yandex.ru>
2022-10-13 12:30:34 +0300

    add 4_proj

--- 3_proj/Program.cs
+++ 3_proj/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите число : ");
+﻿Console.Write("Введите число: ");
 int day = Convert.ToInt32(Console.ReadLine());
 
 if (day > 0 && day < 8 )
@@ -7,32 +7,32 @@ if (day > 0 && day < 8 )
     {
         Console.WriteLine("Понедельник");
     }
-     if (day == 2)
+    else if (day == 2)
     {
         Console.WriteLine("Вторник");
     }
-     if (day == 3)
+    else if (day == 3)
     {
         Console.WriteLine("Среда");
     }
-     if (day == 4)
+    else if (day == 4)
     {
         Console.WriteLine("Четверг");
     }
-     if (day == 5)
+    else if (day == 5)
     {
         Console.WriteLine("Пятница");
     }
-     if (day == 6)
+    else if (day == 6)
     {
         Console.WriteLine("Суббота");
     }
-     if (day == 7)
+    else if (day == 7)
     {
         Console.WriteLine("Воскресенье");
     }
 }
 else
 {  
-    Console.WriteLine("Заданное число не соответствует дню недели");
+    Console.WriteLine($"Заданное число {day} не соответствует дню недели");
 }
---
+++ 4_proj/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+        //count = count + 1;
+    }
+}
+else Console.WriteLine("Введите целое положительное число!");
