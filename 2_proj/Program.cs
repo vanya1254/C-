ca148cadc28a756821b5300ff602b66637f768d4
Ivan <vanya1254@yandex.ru>
2022-10-13 11:31:32 +0300

    add 2_proj

---
+++ 2_proj/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"Квадрат числа {num2} = {num1}");
+}
+else
+{
+    Console.WriteLine("Введены случайные числа");
+}
