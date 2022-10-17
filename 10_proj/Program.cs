021647091c1deefb5505fdcf83df7edc8458afb4
Ivan <vanya1254@yandex.ru>
2022-10-17 10:48:34 +0300

    add 10_proj 2

--- 10_proj/Program.cs
+++ 10_proj/Program.cs
@@ -4,11 +4,16 @@ Console.WriteLine(num);
 int num_1 = num / 10;
 int num_2 = num % 10;
 
-if (num_1 > num_2)
-{
-    Console.WriteLine(num_1);
-}
-else
-{
-    Console.WriteLine(num_2);
-}
+// if (num_1 > num_2)
+// {
+//     Console.WriteLine(num_1);
+// }
+// else
+// {
+//     Console.WriteLine(num_2);
+// }
+
+// int MaxDigit = num_1 > num_2 ? num_1 : num_2;  // тернарный оператор
+// Console.WriteLine(MaxDigit);
+
+Console.WriteLine(num_1 > num_2 ? num_1 : num_2);  // тернарный оператор
