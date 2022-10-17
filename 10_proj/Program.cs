cb22a7e429c7711d1765fbb9eec2a14cca9a099e
Ivan <vanya1254@yandex.ru>
2022-10-17 11:12:44 +0300

    add 10_proj 3

--- 10_proj/Program.cs
+++ 10_proj/Program.cs
@@ -1,8 +1,8 @@
 ﻿int num = new Random().Next(10, 100);
 Console.WriteLine(num);
 
-int num_1 = num / 10;
-int num_2 = num % 10;
+// int num_1 = num / 10;
+// int num_2 = num % 10;
 
 // if (num_1 > num_2)
 // {
@@ -13,7 +13,20 @@ int num_2 = num % 10;
 //     Console.WriteLine(num_2);
 // }
 
+
 // int MaxDigit = num_1 > num_2 ? num_1 : num_2;  // тернарный оператор
 // Console.WriteLine(MaxDigit);
 
-Console.WriteLine(num_1 > num_2 ? num_1 : num_2);  // тернарный оператор
+
+// Console.WriteLine(num_1 > num_2 ? num_1 : num_2);  // тернарный оператор
+
+
+int MaxDigit(int number)
+{
+    int num_1 = number / 10;
+    int num_2 = number % 10;
+    return num_1 > num_2 ? num_1 : num_2;
+}
+
+int res = MaxDigit(num);
+Console.WriteLine(res);
