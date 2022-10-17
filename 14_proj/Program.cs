d0eaa08c0bd0a6f28ff28dfd81513ae4b9fd814b
Ivan <vanya1254@yandex.ru>
2022-10-17 12:35:28 +0300

    add 14_proj 2

--- 14_proj/Program.cs
+++ 14_proj/Program.cs
@@ -7,6 +7,10 @@ if (num1 * num1 == num2)
 {
     Console.WriteLine($"Число {num2} явлется квадратом числа {num1}");
 }
+else if (num2 * num2 == num1)
+{
+    Console.WriteLine($"Число {num1} явлется квадратом числа {num2}");
+}
 else
 {
     Console.WriteLine($"Число {num2} не явлется квадратом числа {num1}");
