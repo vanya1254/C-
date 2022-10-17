da04181f6dd04b882ce5aa99cd07028e8722847d
Ivan <vanya1254@yandex.ru>
2022-10-17 11:47:02 +0300

    add 10_proj 3

--- 11_proj/Program.cs
+++ 11_proj/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int RmSecondNum (int number)
+{
+    int FirstDig = number / 100;
+    int ThirdhDig = number % 10;
+    int num1_3 = FirstDig * 10 + ThirdhDig;
+    Console.WriteLine(number);
+    return num1_3;
+}
+
+int res = RmSecondNum(new Random().Next(100, 1000));
+Console.WriteLine(res);
