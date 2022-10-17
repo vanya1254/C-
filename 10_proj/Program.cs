1261c7e2f9909ba4f621c447973744f16200858b
Ivan <vanya1254@yandex.ru>
2022-10-17 10:36:45 +0300

    add 10_proj

---
+++ 10_proj/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+
+int num_1 = num / 10;
+int num_2 = num % 10;
+
+if (num_1 > num_2)
+{
+    Console.WriteLine(num_1);
+}
+else
+{
+    Console.WriteLine(num_2);
+}
