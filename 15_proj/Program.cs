886a3a58a9ccafcb797990f3a66717c1ea280cf9
Ivan <vanya1254@yandex.ru>
2022-10-18 11:37:57 +0300

    add 15_proj

---
+++ 15_proj/Program.cs
@@ -0,0 +1,17 @@
+﻿int SecondNum (int num)
+{
+    int SecNum = (num % 100) / 10;
+    return SecNum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    int res = SecondNum(number);
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {res}");
+}
+else
+{
+    Console.WriteLine($"{number} не является трехзначным числом!");
+}
