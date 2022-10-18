ea18a6304b69f51cb09c3b30f718e209b1eae6d3
Ivan <vanya1254@yandex.ru>
2022-10-18 13:27:41 +0300

    add 12_proj

--- 12_proj/Program.cs
+++ 12_proj/Program.cs
@@ -1,25 +1,21 @@
-﻿// 12. Напишите программу, которая будет принимать на
-// вход два числа и выводить, является ли второе число
-// кратным первому. Если число 1 не кратно числу 2, то
-// программа выводит остаток от деления.
-// 34, 5 -> не кратно, остаток 4
-// 16, 4 -> кратно
-
-
-int DivNum(int number1, int number2)
+﻿int DivNum(int number1, int number2)
 {
     int Div = number1 % number2;
-    if (Div > 0)
-    {
-        return Div;
-    }
-    else
-    {
-        return Div
-    }
+    return Div;
 }
 
-int res = DivNum(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
+int res = DivNum(num1, num2);
 
-Console.WriteLine(res);
+if (res == 0)
+{
+    Console.WriteLine($"Число {num1} кратно {num2}!");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не кратно {num2}! Остаток от деления {res}.");
+}
