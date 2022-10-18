91614f7a4101612027f9cb3a16a60b1042988662
Ivan <vanya1254@yandex.ru>
2022-10-18 12:18:50 +0300

    add 16_proj

---
+++ 16_proj/Program.cs
@@ -0,0 +1,37 @@
+﻿bool Have3Num (int num)
+{
+    if ((num > 99 && num >= 0) || (num < 0 && num < -99))
+    {
+        return true;
+    }
+    return false;
+}
+
+int Find3Num (int num)
+{
+    while ((num > 999 && num >= 0) || (num < 0 && num < -999))
+    {
+        num = num / 10;
+    }
+    int ThirdNum = num % 10;
+    if (ThirdNum < 0)
+    {
+        return -ThirdNum;
+    }
+    return ThirdNum;
+}
+
+Console.WriteLine("Введите число, лучше трехзначное (вдруг не сработает)");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool resH3N = Have3Num(number);
+
+if (resH3N)
+{
+    int resF3N = Find3Num(number);
+    Console.WriteLine($"Трертья цифра числа {number} равна {resF3N}");
+}
+else
+{
+    Console.WriteLine($"У числа {number} нет третьей цифры");
+}
