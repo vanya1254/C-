38ee6d94610f27745401bb532f73ca467b67df93
Ivan <vanya1254@yandex.ru>
2022-10-18 13:34:30 +0300

    add 11_proj

--- 11_proj/Program.cs
+++ 11_proj/Program.cs
@@ -3,9 +3,11 @@
     int FirstDig = number / 100;
     int ThirdhDig = number % 10;
     int num1_3 = FirstDig * 10 + ThirdhDig;
-    Console.WriteLine(number);
     return num1_3;
 }
 
-int res = RmSecondNum(new Random().Next(100, 1000));
-Console.WriteLine(res);
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Выбрано трехзначное число {num}");
+
+int res = RmSecondNum(num);
+Console.WriteLine($"Преобразовано в двузначное {res}");
