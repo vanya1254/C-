8ad08876cbef1a17447052c0eeb028a2738aad22
Ivan <vanya1254@yandex.ru>
2022-10-20 11:13:50 +0300

    add 19_ptoj

---
+++ 19_proj/Program.cs
@@ -0,0 +1,14 @@
+﻿string Quarter (int quart)
+{
+    if (quart == 1) return $"x > 0 && y > 0";
+    if (quart == 2) return $"x < 0 && y > 0";
+    if (quart == 3) return $"x < 0 && y < 0";
+    if (quart == 4) return $"x > 0 && y < 0";
+    return "Введено некоректное значение";
+}
+
+Console.WriteLine("Введите номер четверти");
+int quart_cons = Convert.ToInt32(Console.ReadLine());
+
+string result = Quarter(quart_cons);
+Console.WriteLine(result);
