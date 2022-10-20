2391d0d63f5edcc5124c1292e25167e83acb8747
Ivan <vanya1254@yandex.ru>
2022-10-20 12:29:49 +0300

    add 21_ptoj 2

--- 21_proj/Program.cs
+++ 21_proj/Program.cs
@@ -1,15 +1,29 @@
-﻿void QuadNum (int quad)
+﻿// void QuadNum (int quad)
+// {
+//     int count = 1;
+//     while (quad >= count)
+//     {
+//         Console.WriteLine($"{count} => {count * count}");
+//         count++;
+//     }
+// }
+
+void QuadNum(int quad)
 {
-    int count = 1;
-    while (quad >= count)
+    if (quad > 0)
     {
-        Console.WriteLine($"{count} => {count * count}");
-        count++;
+        for (int i = 1; i <= quad; i++)
+        {
+            Console.WriteLine($"|{i, 5} | {i * i, 5}|");
+        }
     }
+    else Console.WriteLine("Введите значение больше 0");
 }
 
 Console.Write("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num > 0) QuadNum(num);
-else Console.WriteLine("Введите натуральное число!");
+QuadNum(num);
+
+// if (num > 0) QuadNum(num);
+// else Console.WriteLine("Введите натуральное число!");
