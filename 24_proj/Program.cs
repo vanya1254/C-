9ad2e78c5b1f5791ba2f739a6540a8a8951ef251
Ivan <vanya1254@yandex.ru>
2022-10-21 12:45:37 +0300

    add 24_proj

---
+++ 24_proj/Program.cs
@@ -0,0 +1,20 @@
+﻿void CubedNum(int cube)
+{
+    if (cube > 0)
+    {
+        for (int i = 1; i <= cube; i++)
+        {
+            Console.WriteLine($"|{i, 4} | {Math.Pow(i, 3), 5}|");
+        }
+    }
+    else Console.WriteLine("Введите значение больше 0");
+}
+
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+CubedNum(num);
