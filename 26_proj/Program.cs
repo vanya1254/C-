876942237948687c0a0f6cb6b67543fa8e0e5e82
Ivan <vanya1254@yandex.ru>
2022-11-07 11:37:26 +0300

    add 26_proj

--- 26_proj/Program.cs
+++ 26_proj/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("");
+﻿int CountNum(int num)
+{
+    int count = 0;
+
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+// int CountNum(int num)
+// {
+//     int count = 0;
+
+//     for (int i = num; i > 0; i /= 10)
+//     {
+//         count++;
+//     }
+//     return count;
+// }
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int countNum = CountNum(number);
+
+Console.WriteLine($"У вашего числа {countNum} цифры");
