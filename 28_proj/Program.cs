30646091eb4a9817fa7cf28a599079433e67b855
Ivan <vanya1254@yandex.ru>
2022-11-08 10:09:02 +0300

    add 28_proj changes

--- 28_proj/Program.cs
+++ 28_proj/Program.cs
@@ -1,15 +1,27 @@
-﻿void MassiveZeroOne(int num)
+﻿int[] MassiveZeroOne(int num)
 {
     int[] array = new int [num];
 
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(0, 2);
-        Console.Write($"{array[i]}, ");
     }
+
+    return array;
+}
+
+void MassivePrint (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
 }
 
 Console.WriteLine("Введите количество элементов в массиве");
 int number = Convert.ToInt32(Console.ReadLine());
 
-MassiveZeroOne(number);
+int[] rndArray = MassiveZeroOne(number);
+MassivePrint(rndArray);
