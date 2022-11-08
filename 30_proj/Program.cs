6d8a0672dc225928b00d5b714eee60cefd1190fc
Ivan <vanya1254@yandex.ru>
2022-11-08 11:09:37 +0300

    add 30_proj

---
+++ 30_proj/Program.cs
@@ -0,0 +1,19 @@
+﻿int SumDigits(int num)
+{
+    int sum = default;
+    num = Math.Abs(num);
+    
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigits(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {sumDigits}");
