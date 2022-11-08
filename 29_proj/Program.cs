cbd9fbe88d62cb2c1d7880fcddc44a2d3accb50f
Ivan <vanya1254@yandex.ru>
2022-11-08 10:58:02 +0300

    add 29_proj

--- 29_proj/Program.cs
+++ 29_proj/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double Exponentiation(double num, int degree)
+{
+    double exponentiation = 1;
+
+    for (int i = 0; i < degree; i++)
+    {
+        exponentiation *= num;
+    }
+
+    return exponentiation;
+}
+
+bool IsNatural(int number)
+{
+    if (number > 0) return true;                                             //  проверка на натуральность степени
+    return false;
+}
+
+bool condition = true;
+
+Console.WriteLine("Введите число, которое хотите возвести в степень");
+double a = Convert.ToDouble(Console.ReadLine());                             //  использовал тип double т.к. сказано что только степень натуральная
+
+while (condition)
+{
+    Console.WriteLine("Введите натуральную степень");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    if (IsNatural(b))
+    {
+        double expoA = Exponentiation(a, b);
+        System.Console.WriteLine($"Число {a} в степени {b} равно => {expoA}");
+        condition = false;
+    }
+}
