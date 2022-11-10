aacab377127c6dc82776bb0b681e842b567aeef7
Ivan <vanya1254@yandex.ru>
2022-11-10 12:11:12 +0300

    add 35_proj

---
+++ 35_proj/Program.cs
@@ -0,0 +1,40 @@
+﻿int[] RndMass(int size, int mn, int mx)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(mn, mx + 1);        
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int GetCountElem(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99) count++;
+    }
+
+    return count;
+}
+
+int[] arr = RndMass(123, 0, 200);
+PrintArray(arr);
+int counter = GetCountElem(arr);
+Console.WriteLine($"Количество элементов от 10 до 99 => {counter}");
