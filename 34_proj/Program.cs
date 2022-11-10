194b9d35e4f64e16f3783a46e6b7750b96403544
Ivan <vanya1254@yandex.ru>
2022-11-10 11:28:00 +0300

    add 34_proj

---
+++ 34_proj/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] RndMass(int size, int mn, int mx)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(mn, mx + 1);        
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNUmArray(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num) return true;
+    }
+
+    return false;
+}
+
+Console.WriteLine("Введите искомое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arr = RndMass(12, -9, 9);
+PrintArray(arr);
+
+if (FindNUmArray(arr, number)) 
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
