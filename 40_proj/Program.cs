67ef4d189cb741116cd72d2ff5df794c1b6e5292
Ivan <vanya1254@yandex.ru>
2022-11-14 10:27:18 +0300

    add 40_proj

---
+++ 40_proj/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] RndMass(int size, int mn, int mx)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(mn, mx + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+int[] arr = RndMass(5, 1, 9);
+PrintArray(arr);
+ReverseArray(arr);
+PrintArray(arr);
