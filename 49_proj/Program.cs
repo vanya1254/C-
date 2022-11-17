f933e572fcecd760c7d49e3267408cff524a88b9
Ivan <vanya1254@yandex.ru>
2022-11-17 11:40:25 +0300

    add 49_proj

---
+++ 49_proj/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] CreateRndMatrix(int rows, int columns, int mn, int mx)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(mn, mx + 1);
+        }
+    }
+
+    return matrix;
+}
+
+int[,] FindSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixRnd = CreateRndMatrix(3, 4, -10, 10);
+PrintMatrix(matrixRnd);
+Console.WriteLine();
+int[,] matrixSquare = FindSquare(matrixRnd);
+PrintMatrix(matrixSquare);
