cfd93a92355964a67c6ae7b320d6cb9477d635f8
Ivan <vanya1254@yandex.ru>
2022-11-17 11:09:17 +0300

    add 48_proj

---
+++ 48_proj/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixIJ = CreateMatrix(3, 4);
+PrintMatrix(matrixIJ);
