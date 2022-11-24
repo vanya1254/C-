4f9b5d47cae1696b43be8a0cda5ce949b70a5365
Ivan <vanya1254@yandex.ru>
2022-11-24 11:06:34 +0300

    add 63-64_proj

---
+++ 63_proj/Program.cs
@@ -0,0 +1,21 @@
+﻿void NaturalNums(int n)
+{
+    if (n == 0) return;
+    NaturalNums(n - 1);
+    Console.Write($"{n} ");
+}
+
+Console.WriteLine("Введите число элементов последовательности");
+int num = Convert.ToInt32(Console.ReadLine());
+NaturalNums(num);
+
+// void NaturalNumbers(int num)
+// {
+//     if (num > 0)
+//     {
+//         NaturalNumbers(num - 1);
+//         Console.Write($"{num} ");
+//     }
+// }
+
+// NaturalNumbers(number);
---
+++ 64_proj/Program.cs
@@ -0,0 +1,25 @@
+﻿void NaturalNums(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m} ");
+        NaturalNums(m + 1, n);
+    }
+
+    if (m > n)
+    {
+        Console.Write($"{m} ");
+        NaturalNums(m - 1, n);
+    }
+
+    if (m == n)
+    {
+        Console.Write($"{m} ");
+    }
+}
+
+Console.WriteLine("Введите первое число последовательности");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число последовательности");
+int num2 = Convert.ToInt32(Console.ReadLine());
+NaturalNums(num1, num2);
