f153b26c1e6de59c529db2da7cace1cdd99bde60
Ivan <vanya1254@yandex.ru>
2022-11-24 11:48:58 +0300

    add 65-66_proj

---
+++ 65_proj/Program.cs
@@ -0,0 +1,11 @@
+﻿int SumDigitalsNum(int num)
+{
+    if (num > 0) return num % 10 + SumDigitalsNum(num / 10);
+    return 0;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigitalsNum(number);
+Console.WriteLine(sumDigits);
---
+++ 66_proj/Program.cs
@@ -0,0 +1,14 @@
+﻿int PowAToB(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return PowAToB(numA, numB - 1) * numA;
+}
+
+Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int powAToB = PowAToB(numberA, numberB);
+Console.WriteLine(powAToB);
