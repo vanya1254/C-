a40328c61a9fc836df8ea80db1041971104cd43c
Ivan <vanya1254@yandex.ru>
2022-11-25 17:10:57 +0300

    update 69_proj

--- 62_proj/Program.cs
+++ 62_proj/Program.cs
@@ -4,3 +4,38 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+int[,] CreateSpiralMatrix(int rows, int columns, int mn, int mx)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int k = 1; k < matrix.Length; k++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = k++;
+            }
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}  |");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] matrixSpiral = CreateSpiralMatrix(4, 4, 1, 16);
+PrintMatrix(matrixSpiral);
--- 69_proj/Program.cs
+++ 69_proj/Program.cs
@@ -2,9 +2,8 @@
 {
     if (m == 0) return n + 1;
     if (m > 0 && n == 0) return GetAkkermanFunction(m - 1, 1);                               // перевел в код написаное в википедии про функцию, но так и не понял, для чего она
-    if (m > 0 && n > 0) return GetAkkermanFunction(m - 1, GetAkkermanFunction(m, n - 1));    // https://wikimedia.org/api/rest_v1/media/math/render/svg/c8c2aa0b20532014ea35c4a09c2380a01b3d1423
-
-    return GetAkkermanFunction(m, n);
+    
+    return GetAkkermanFunction(m - 1, GetAkkermanFunction(m, n - 1));    // https://wikimedia.org/api/rest_v1/media/math/render/svg/c8c2aa0b20532014ea35c4a09c2380a01b3d1423
 }
 
 Console.WriteLine("Введите M");
