2154931a4f79f7a4cdd26c18b60cab1bd87273a0
Ivan <vanya1254@yandex.ru>
2022-11-25 12:29:19 +0300

    add 68_proj

---
+++ 68_proj/Program.cs
@@ -0,0 +1,18 @@
+﻿int NaturalsSequenceSum(int m, int n)
+{
+    int sum = default;
+
+    if (m < n) sum += m + NaturalsSequenceSum(m + 1, n);
+    if (m > n) sum += m + NaturalsSequenceSum(m - 1, n);
+    if (m == n) sum += m;
+    
+    return sum;
+}
+
+Console.WriteLine("Введите M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int sumSequence = NaturalsSequenceSum(numberM, numberN);
+Console.WriteLine($"Сумма чисел равна => {sumSequence}");
