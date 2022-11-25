c16f2e275353f3c7e0be00d08c8f70706d1cf50e
Ivan <vanya1254@yandex.ru>
2022-11-25 12:13:25 +0300

    add 67_proj

---
+++ 67_proj/Program.cs
@@ -0,0 +1,10 @@
+﻿void NaturalsSequence(int n)
+{
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    NaturalsSequence(n - 1);
+}
+
+Console.WriteLine("Введите первое число последовательности");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalsSequence(number);
