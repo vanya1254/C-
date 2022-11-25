2e5bcd950924e29a30a01eff6859364a2b06b7a7
Ivan <vanya1254@yandex.ru>
2022-11-25 19:01:34 +0300

    update 62_proj

--- 62_proj/Program.cs
+++ 62_proj/Program.cs
@@ -5,19 +5,17 @@
 // 11 16 15 06
 // 10 09 08 07
 
-int[,] CreateSpiralMatrix(int rows, int columns, int mn, int mx)
+int[,] CreateSpiralMatrix(int rows, int columns)
 {
     int[,] matrix = new int[rows, columns];
+    int number = 1;
 
-    for (int k = 1; k < matrix.Length; k++)
+    for (int i = 0; i < matrix.Length / 2; i++)
     {
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                matrix[i, j] = k++;
-            }
-        }
+        for (int j = i; j < matrix.Length - i; j++) matrix[i, j] = number++;
+        for (int k = i + 1; k < matrix.Length - i; k++) matrix[k, matrix.Length - 1 - i] = number++;
+        for (int j = matrix.Length - i - 2; j >= i; j--) matrix[matrix.Length - 1 - i, j] = number++;
+        for (int k = matrix.Length - 2 - i; k > i; k--) matrix[k, i] = number++;
     }
 
     return matrix;
@@ -37,5 +35,5 @@ void PrintMatrix(int[,] matrix)
     Console.WriteLine();
 }
 
-int[,] matrixSpiral = CreateSpiralMatrix(4, 4, 1, 16);
+int[,] matrixSpiral = CreateSpiralMatrix(5, 5);
 PrintMatrix(matrixSpiral);
--- 69_proj/Program.cs
+++ 69_proj/Program.cs
@@ -1,7 +1,7 @@
 ﻿int GetAkkermanFunction(int m, int n)
 {
     if (m == 0) return n + 1;
-    if (m > 0 && n == 0) return GetAkkermanFunction(m - 1, 1);                               // перевел в код написаное в википедии про функцию, но так и не понял, для чего она
+    if (m > 0 && n == 0) return GetAkkermanFunction(m - 1, 1);           // перевел в код написаное в википедии про функцию, но так и не понял, для чего она
     
     return GetAkkermanFunction(m - 1, GetAkkermanFunction(m, n - 1));    // https://wikimedia.org/api/rest_v1/media/math/render/svg/c8c2aa0b20532014ea35c4a09c2380a01b3d1423
 }
